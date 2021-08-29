9960e79a9c381415ad09af8ecf377925adf89f41
Hok S. Uy <hoksuy@gmail.com>
2021-08-29 13:41:29 -0700

    working save note

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const notes = require('../../db/db');
+const { createNote, removeNote, validateNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    console.log(notes);
+    console.log(req.body);
+    if(validateNote(req.body))
+        createNote(notes, req.body);
+    else
+        console.log("Not a valid note.");
 });
 
 module.exports = router;
